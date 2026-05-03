fe5c9d5b1b776834c618475fd658e45b9bfc9002
Ts-João <cttjoaovts@gmail.com>
2026-05-03 17:43:04 -0300

    feat: adiciona botao de exibir dica e exibidor de dificuldade

--- scenes/gameplay/quiz/quiz_screen.gd
+++ scenes/gameplay/quiz/quiz_screen.gd
@@ -6,6 +6,8 @@ func _ready() -> void:
 	QuizManager.answer_wrong.connect(_on_answer_wrong)
 	QuizManager.quiz_completed.connect(_on_quiz_completed)
 	QuizManager.load_quiz("res://data/quizzes/quiz_ato_ii.json")
+	$ButtonHint.pressed.connect(_on_hint_pressed)
+	$ButtonHint.text = "Dica"
 	$HBoxContainer/ButtonA.pressed.connect(func(): QuizManager.submit_answer("A"))
 	$HBoxContainer/ButtonB.pressed.connect(func(): QuizManager.submit_answer("B"))
 	$HBoxContainer/ButtonC.pressed.connect(func(): QuizManager.submit_answer("C"))
@@ -14,9 +16,13 @@ func _ready() -> void:
 	$ButtonContinue.pressed.connect(_on_continue_pressed)
 	pass
 
+func _on_hint_pressed() -> void:
+	$HintLabel.visible = true
+
 func _on_question_loaded(data: Dictionary) -> void:
 	$QuizLabel.text = data["text"]
 	$HintLabel.text = data["hint"]
+	$DifficultyLabel.text = "Dificuldade: " + data["level"]
 	$HBoxContainer/ButtonA.text = data["options"][0]["text"]
 	$HBoxContainer/ButtonB.text = data["options"][1]["text"]
 	$HBoxContainer/ButtonC.text = data["options"][2]["text"]
@@ -33,6 +39,7 @@ func _on_answer_correct(feedback: String) -> void:
 	pass
 
 func _on_continue_pressed() -> void:
+	$HintLabel.visible = false
 	$ButtonContinue.visible = false
 	$FeedbackLabel.visible = false
 	$HBoxContainer/ButtonA.visible = true 
